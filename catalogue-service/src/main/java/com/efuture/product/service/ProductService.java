fb6d61d44f5a8872763f7dd1969d8b6ea2026aed
Supimi <supimi.15@cse.mrt.ac.lk>
2025-02-23 15:49:19 +0530

    Add comments

--- catalogue-service/src/main/java/com/efuture/product/service/ProductService.java
+++ catalogue-service/src/main/java/com/efuture/product/service/ProductService.java
@@ -42,14 +42,23 @@ public class ProductService {
         this.kafkaProducerService = kafkaProducerService;
     }
 
+    /**
+     * Save products to db
+     * @param createProductRequest product create request
+     * @return product creation response
+     */
     public Response<ProductInformation> createProduct(CreateProductRequest createProductRequest) {
         log.info("create product request:{}", createProductRequest);
+        //map the request to db record and save
         Product product = productMapper.mapToProduct(createProductRequest);
         product.setStatus(ACTIVE.getValue());
         Product saved = productRepository.save(product);
+
+        //map the request to kafka event message and send it into the configured topic
         ProductInformation productInformation = productMapper.mapToProductInformation(saved);
         ProductCreationEvent event = productMapper.mapToProductCreationEvent(saved);
         kafkaProducerService.sendMessage(event);
+
         return Response.<ProductInformation>builder()
                 .status(STATUS_CREATED)
                 .message(SUCCESS)
@@ -57,8 +66,16 @@ public class ProductService {
                 .build();
     }
 
+    /**
+     * Update existing products
+     * @param productId Product Id
+     * @param updateProductRequest Update product request
+     * @return Update product response
+     */
     public Response<ProductInformation> updateProduct(Long productId, UpdateProductRequest updateProductRequest) {
         log.info("update product request:{}", updateProductRequest);
+
+        //find the product
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new InvalidRequestException(PRODUCT_NOT_FOUND,
                         String.format(PRODUCT_NOT_FOUND_DESCRIPTION, productId)));
@@ -66,9 +83,13 @@ public class ProductService {
             throw new InvalidRequestException(INVALID_PRODUCT_ID,
                     String.format(PRODUCT_ALREADY_DELETED, productId));
         }
+        //map update product request body to existing product record
         productMapper.mapUpdateRequestToProduct(updateProductRequest, product);
         Product saved = productRepository.save(product);
+
+        //map to response data
         ProductInformation productInformation = productMapper.mapToProductInformation(saved);
+
         return Response.<ProductInformation>builder()
                 .status(STATUS_OK)
                 .message(SUCCESS)
@@ -76,12 +97,21 @@ public class ProductService {
                 .build();
     }
 
+    /**
+     * Delete products (Soft Delete) -mark product status as 'D'
+     * @param productId product id
+     * @return response data
+     */
     public Response<ProductInformation> deleteProduct(Long productId) {
         log.info("delete product request:{}", productId);
+
+        //retrieve the existing product in the db
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new InvalidRequestException(PRODUCT_NOT_FOUND,
                         String.format(PRODUCT_NOT_FOUND_DESCRIPTION, productId)));
+
         String message;
+        //handle already deleted products
         if (DELETED.getValue().equals(product.getStatus())) {
             log.info("product is already deleted");
             message = String.format(PRODUCT_ALREADY_DELETED, productId);
@@ -91,6 +121,7 @@ public class ProductService {
             message = SUCCESS;
         }
 
+        //map to response data
         ProductInformation productInformation = productMapper.mapToProductInformation(product);
         return Response.<ProductInformation>builder()
                 .status(STATUS_OK)
@@ -99,9 +130,17 @@ public class ProductService {
                 .build();
     }
 
+    /**
+     * Retrieve all products by category
+     * @param category category
+     * @return product list
+     */
     public Response<List<ProductInformation>> getProductByCategory(String category) {
         log.info("get active products by category:{}", category);
+
+        //find active products by category
         List<Product> products = productRepository.findByCategoryAndStatus(category, ACTIVE.getValue());
+        //map to response list
         List<ProductInformation> productInformationList = productMapper.mapToProductInformationList(products);
         return Response.<List<ProductInformation>>builder()
                 .status(STATUS_OK)
@@ -110,10 +149,17 @@ public class ProductService {
                 .build();
     }
 
+    /**
+     * Retrieve premium products
+     * If the product price is greater than 500, it is considered as premium product
+     * @return product list
+     */
     public Response<List<ProductInformation>> getPremiumProducts() {
         log.info("get active premium products");
+        //find all active premium products
         List<Product> products = productRepository
                 .findByStatusAndPriceGreaterThanEqualOrderByPrice(ACTIVE.getValue(), PREMIUM_PRODUCT_PRICE_LIMIT);
+        //map to response
         List<ProductInformation> productInformationList = productMapper.mapToProductInformationList(products);
         return Response.<List<ProductInformation>>builder()
                 .status(STATUS_OK)
